2abc992ba07f2f1a0a29eca5c142210d06a6b946
Tamego <oitrosutuik@gmail.com>
2024-09-04 22:30:37 +0900

    合計得点でソートできるように #7

--- ui/display_window/display_window.gd
+++ ui/display_window/display_window.gd
@@ -8,5 +8,28 @@ func _ready() -> void:
 func update_element() -> void:
 	(get_node("%EventName") as Label).text = DataManager.event_name
 	
+	var sort_total_score: Array = []
+	
 	for i in range(7):
-		(get_node("%TotalScore/%Class" + str(i + 1)) as Label).text = str(DataManager.total_score[i]) + "点"
+		sort_total_score.append([DataManager.total_score[i], i])
+	
+	sort_total_score.sort_custom(
+		func(a, b) -> bool:
+			if a[0] > b[0]:
+				return true
+			elif a[0] < b[0]:
+				return false
+			else:
+				if a[1] < b[1]:
+					return true
+				else:
+					return false
+	)
+	
+	for i in range(7):
+		var score: int = sort_total_score[i][0]
+		var class_num: int = sort_total_score[i][1]
+		var score_label: ScoreLabel = (get_node("%TotalScore/%Class" + str(class_num + 1)) as ScoreLabel)
+		
+		score_label.get_parent().move_child(score_label, i)
+		score_label.score.text = str(DataManager.total_score[class_num]) + "点"
---
+++ ui/score_label/score_label.gd
@@ -0,0 +1,5 @@
+extends HBoxContainer
+class_name ScoreLabel
+
+@onready var team_name: Label = get_node("TeamName")
+@onready var score: Label = get_node("Score")
