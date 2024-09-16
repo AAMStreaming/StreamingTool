3d16b9fc7b341866e0c53853bbccfd31dbd626a5
Tamego <oitrosutuik@gmail.com>
2024-09-16 19:14:57 +0900

    競技名を選択制に#9

--- ui/operation_window/operation_window.gd
+++ ui/operation_window/operation_window.gd
@@ -2,7 +2,8 @@ extends Control
 
 
 func _on_update_button_pressed():
-	DataManager.event_name = (get_node("%Event") as LineEdit).text
+	var event_name: OptionButton = (get_node("%Event") as OptionButton)
+	DataManager.event_name = event_name.get_item_text(event_name.get_selected_id())
 	for i in range(7):
 		DataManager.total_score[i] = int((
 			get_node("%TotalScoreContainer/%Class" + str(i + 1)) as SpinBox
